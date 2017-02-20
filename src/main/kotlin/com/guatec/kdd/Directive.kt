bcfb8899a3059dabf13f7f9fde1ec920ead51581
Juan Liska <jpliska@gmail.com>
2017-02-20 05:46:10 -0600

    shorten java type descriptions

--- src/main/kotlin/com/guatec/kdd/Directive.kt
+++ src/main/kotlin/com/guatec/kdd/Directive.kt
@@ -10,23 +10,35 @@ data class Directive(val name: String, val description: String, val returnType: 
     companion object {
         fun build(type: DirectiveType, method: Method, altName: String? = null) : Directive {
             fun getName(): String = altName ?: method.name
+
             fun getSymbol() = when (type.isExtension()) {
                 true -> "+"
                 false -> "-"
             }
+
+            fun Class<*>.shortName() = canonicalName.replace(Regex("java\\..*\\."), "")
+
             fun getTypeInfo(): String {
-                val paramNames = method.parameters.filterNot { it.type.canonicalName.startsWith("kotlin.jvm.functions.Function") }
-                        .map { it.type.canonicalName }
+                val paramTypes = method.parameters.filterNot { it.type.canonicalName.startsWith("kotlin.jvm.functions.Function") }
+                        .map { it.type.shortName() }
+                var returnType = method.returnType.shortName()
+
+                //parameter types (eg: E) are useless
+                //if (method.returnType.typeParameters.isNotEmpty()) {
+                //    returnType += "<${method.returnType.typeParameters.map{ it.name }.joinToString(", ")}>"
+                //}
+
                 if (type.isProp()) {
-                    return ": ${method.returnType.canonicalName}"
+                    return ": $returnType"
                 }
-                else if (paramNames.isEmpty()) {
-                    return ": ${method.returnType.canonicalName}"
+                else if (paramTypes.isEmpty()) {
+                    return ": $returnType"
                 }
                 else {
-                    return " (${paramNames.joinToString(",")}): ${method.returnType.canonicalName}"
+                    return " (${paramTypes.joinToString(",")}): $returnType"
                 }
             }
+
             return Directive(getName(), getSymbol() + " " + getName() + getTypeInfo(), method.returnType)
         }
     }
